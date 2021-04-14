68820c1afce92cbe666dc27c0a4ed589d91b6c19
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2021-04-15 00:55:22 +0300

    add form

--- src/services/movies-api.js
+++ src/services/movies-api.js
@@ -1,3 +1,1 @@
-
-
 export const apiKey = '2986a356ef3a214eb0a4615eddf8ffa1';
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,23 +2,41 @@ import { Component } from 'react'
 
 class HomeView extends Component {
     state = {
-        movies: [],
+        query: '',
     }
-    async componentDidMount() {
 
+    handleChange = e => {
+        this.setState({ query: e.currentTarget.value })
     }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        // console.log(this.state);
+        this.props.onSubmit(this.state.query)
+        this.setState({ query: '' })
+    }
+
     render() {
 
         return (
             <>
-                <h1>Search</h1>
+                <header >
+                    <form onSubmit={this.handleSubmit}>
 
+                        <input
+                            type="text"
+                            value={this.state.query}
+                            onChange={this.handleChange}
+                        />
+
+                        <button type="submit" >
+                            <span >Search</span>
+                        </button>
+
+                    </form>
+                </header>
             </>)
-
-
-
     }
-
 }
 
 export default HomeView
